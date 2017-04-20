e6d383770abf7bc1135dcb4355635e8f67d0bd42
rodhoward <rod.howard@gmail.com>
2017-04-19 22:09:52 -0700

    Adding first Spring boot project with initial test hello world.

---
+++ src/main/java/rod/Application.java
@@ -0,0 +1,34 @@
+package rod;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(final ApplicationContext ctx) {
+
+        return new CommandLineRunner() {
+            public void run(String... strings) throws Exception {
+                String[] beanNames = ctx.getBeanDefinitionNames();
+                Arrays.sort(beanNames);
+                for (String beanName : beanNames) {
+                    System.out.println(beanName);
+                    String v = "this shouldn't be saved.";
+                }
+            }
+        };
+
+    }
+
+
+}
